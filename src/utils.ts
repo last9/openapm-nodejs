a2471d2aea593a7d64f5092848542eb5c3c40789
Tushar Choudhari <tushar@last9.io>
2023-07-18 13:53:46 +0530

    Use originalUrl because .path is not present in some cases

--- src/utils.ts
+++ src/utils.ts
@@ -1,15 +1,15 @@
-import * as fs from "fs";
-import * as path from "path";
-import * as os from "os";
-import UrlValueParser from "url-value-parser";
+import * as fs from 'fs';
+import * as path from 'path';
+import * as os from 'os';
+import UrlValueParser from 'url-value-parser';
 
 export const getPackageJson = () => {
-  const packageJsonPath = path.join(process.cwd(), "package.json");
+  const packageJsonPath = path.join(process.cwd(), 'package.json');
   try {
-    const packageJson = fs.readFileSync(packageJsonPath, "utf-8");
+    const packageJson = fs.readFileSync(packageJsonPath, 'utf-8');
     return JSON.parse(packageJson);
   } catch (error) {
-    console.error("Error parsing package.json");
+    console.error('Error parsing package.json');
     return null;
   }
 };
@@ -20,10 +20,10 @@ export const getHostIpAddress = () => {
   // Iterate over network interfaces to find a non-internal IPv4 address
   for (const interfaceName in networkInterfaces) {
     const interfaces = networkInterfaces[interfaceName];
-    if (typeof interfaces !== "undefined") {
+    if (typeof interfaces !== 'undefined') {
       for (const iface of interfaces) {
         // Skip internal and non-IPv4 addresses
-        if (!iface.internal && iface.family === "IPv4") {
+        if (!iface.internal && iface.family === 'IPv4') {
           return iface.address;
         }
       }
@@ -37,8 +37,16 @@ export const getHostIpAddress = () => {
 export const getParsedPathname = (
   pathname: string,
   extraMasks?: Array<RegExp>,
-  replacement: string = ":id"
+  replacement: string = ':id'
 ) => {
   const parser = new UrlValueParser({ extraMasks });
   return parser.replacePathValues(pathname, replacement);
+};
+
+export const getSanitizedPath = (pathname: string) => {
+  const sanitizedPath = pathname.replace(
+    /(\/[^?#]+)(?:\?[^#]*)?(?:#.*)?$/,
+    '$1'
+  );
+  return sanitizedPath;
 };
